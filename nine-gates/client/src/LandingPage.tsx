1f884a4708e6cb08d1a36a5979c57cd778abdeaf
ahjamshidi <a.hossein.jamshidi@gmail.com>
2024-06-13 17:59:19 +0200

    add Router and seprate main page from app

--- nine-gates/client/src/LandingPage.tsx
+++ nine-gates/client/src/LandingPage.tsx
@@ -1,32 +1,17 @@
-import * as React from 'react';
-import PropTypes from 'prop-types';
-import CssBaseline from '@mui/material/CssBaseline';
+
 import Box from '@mui/material/Box';
 import Divider from '@mui/material/Divider';
-import { ThemeOptions, ThemeProvider, createTheme } from '@mui/material/styles';
-import AppAppBar from './components/AppAppBar';
 import Hero from './components/Hero';
 import FAQ from './components/FAQ';
 import Footer from './components/Footer';
-import getLPTheme from './components/getLPTheme';
-import { PaletteMode } from '@mui/material';
+
 
 export default function LandingPage() {
-  const initPaletteMode: PaletteMode = 'light';
-  const [mode, setMode] = React.useState<PaletteMode>(initPaletteMode);
-  const [showCustomTheme, setShowCustomTheme] = React.useState(false);
-  const LPtheme = createTheme(getLPTheme(mode) as ThemeOptions);
-  const defaultTheme = createTheme({ palette: { mode } });
-
-  const toggleColorMode = () => {
-    setMode((prev) => (prev === 'dark' ? 'light' : 'dark'));
-  };
-//TODO: move them provider to main page
-//TODO: move them AppAppBar to main page
+  
+  //TODO: move them provider to main page
+  //TODO: move them AppAppBar to main page
   return (
-    <ThemeProvider theme={showCustomTheme ? LPtheme : defaultTheme}>
-      <CssBaseline />
-      <AppAppBar mode={mode} toggleColorMode={toggleColorMode} />
+    <>
       <Hero />
       <Box sx={{ bgcolor: 'background.default' }}>
         <Divider />
@@ -37,6 +22,6 @@ export default function LandingPage() {
         <Divider />
         <Footer />
       </Box>
-    </ThemeProvider>
+    </>
   );
 }
--- nine-gates/client/src/main.tsx
+++ nine-gates/client/src/main.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import LandingPage from './LandingPage';
+import App from './app';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <LandingPage />
+    <BrowserRouter>
+      <App></App>
+    </BrowserRouter>
   </React.StrictMode>
 );
