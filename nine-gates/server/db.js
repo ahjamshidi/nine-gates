4186577207468d61b0f8140cb4f0a3762bbbafae
ahjamshidi <a.hossein.jamshidi@gmail.com>
2024-06-13 14:20:47 +0200

    install typescript

--- nine-gates/server/db.js
+++ nine-gates/server/db.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose');
 
 function connectDB() {
-  mongoose.connect('mongodb://localhost:27017/jobSkillsDB')
+  mongoose.connect('mongodb://root:123@localhost:27017/jobSkillsDB?authSource=admin')
     .then(() => console.log('Database connected successfully'))
     .catch(err => console.error('Database connection error: ', err));
 }
