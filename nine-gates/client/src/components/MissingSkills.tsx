c233114a2c7a9824f7f38ecf6868211790fa8a6a
ahjamshidi <a.hossein.jamshidi@gmail.com>
2024-06-13 17:38:58 +0200

    add type directory

---
+++ nine-gates/client/src/components/MissingSkills.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import '../styles/SkillsList.css';
+
+const MissingSkills = ({ currentSkills, desiredSkills, onSkillClick }:{
+  currentSkills:string[],
+  desiredSkills:string[],
+  onSkillClick:(skill:string)=>Promise<void>
+}) => {
+  const missingSkills = desiredSkills.filter(
+    (skill) => !currentSkills.includes(skill)
+  );
+
+  return missingSkills.length > 0 ? (
+    <div>
+      <ul>
+        {missingSkills.map((skill, index) => (
+          <li
+            key={index}
+            style={{ '--i': index }}
+            onClick={() => onSkillClick(skill)}
+          >
+            {skill}
+          </li>
+        ))}
+      </ul>
+    </div>
+  ) : (
+    <div></div>
+  );
+};
+
+export default MissingSkills;
