69c7df35dc288d24145979993d08fbc80a9cfaae
ildefonso-nuno <ildefonso.nuno@gmail.com>
2024-06-14 15:56:14 +0200

    updating seed script

--- nine-gates/server/db.ts
+++ nine-gates/server/db.ts
@@ -3,8 +3,12 @@ import dotenv from 'dotenv';
 dotenv.config();
 function connectDB(): void {
   mongoose
-    .connect(process.env['DATABASE_URI'] || 'mongodb://root:root@localhost:27017/jobSkillsDB?authSource=admin')
+    .connect(
+      process.env['DATABASE_URI'] ||
+        'mongodb://root:root@localhost:27017/jobSkillsDB?authSource=admin'
+    )
     .then(() => console.log('Database connected successfully'))
+    .then(() => console.log('Database: ', process.env['DATABASE_URI']))
     .catch((err) => console.error('Database connection error: ', err));
 }
 
